c77207c974467b5dd19a8eb4b60ac0b8ed2828ff
ErDoue <141413877+ErDoue@users.noreply.github.com>
2023-08-04 19:06:53 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ var config = {
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
         "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "心爱的小可爱",  // 同上...
+        "可爱",  // 同上...
         "今天是你的生日",
         "这是我们在一起的",
         "第三个生日了哦",
@@ -33,9 +33,9 @@ var config = {
         turn_on: "开始",
         play: "音乐",
         bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
+        balloons_flying: "好像少点什么",
+        cake_fadein: "cake？",
+        light_candle: "candle？",
         wish_message: "生日快乐",
         story: "A MESSAGE FOR YOU",
     }
